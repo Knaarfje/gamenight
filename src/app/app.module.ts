3bba1bba7fc96cd8e99350a17cdbfdd7ca9c607a
Thomas Bosch <thomasjbosch@gmail.com>
2017-09-12 14:59:59 +0200

    first!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase, AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB9mgqm5TLz1aA7E8-dxUw9VGjCiaIXkQU',
+  authDomain: 'gamenight-db5de.firebaseapp.com',
+  databaseURL: 'https://gamenight-db5de.firebaseio.com',
+  projectId: 'gamenight-db5de',
+  storageBucket: '',
+  messagingSenderId: '702799739538'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
